15a6133ee701d7f10995872d271a086c793a16a9
Mirco <hi@mirco.space>
2021-02-03 18:44:16 +0800

    forth commit

---
+++ slices/HeroSection/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <section
+    :style="{ backgroundImage: 'url(' + slice.primary.image.url + ')'}"
+    class="hero is-primary is-halfheight"
+  >
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="mb-6 is-primary title">{{slice.primary.title}}</h1>
+        <b-button
+          v-if="slice.primary.link"
+          tag="nuxt-link"
+          :to="slice.primary.link.url"
+          type="is-primary"
+        >{{slice.primary.buttonText}}</b-button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+.hero {
+   {
+  }
+}
+.section {
+  position: relative;
+  background: #f7f7f7;
+  color: #111;
+  padding: 4em;
+  text-align: center;
+}
+a {
+  color: #111;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
---
+++ slices/ImageGallery/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="section container my-6">
+    <h1 class="mb-6 title">{{slice.primary.title}}</h1>
+
+    <b-modal v-model="isImageModalActive">
+      <img v-if="currentImage" :src="currentImage.image.url" />
+    </b-modal>
+    <div class="columns">
+      <div v-for="(item, i) in slice.items" :key="i" class="column has-cursor-pointer">
+        <img
+          class="has-pointer-cursor"
+          @click="currentImage = item, isImageModalActive = true"
+          :src="item.image.url"
+        />
+      </div>
+    </div>
+    <b-button
+      v-if="slice.primary.link"
+      tag="nuxt-link"
+      :to="slice.primary.link.url"
+      type="is-primary"
+    >Vedi Tutti</b-button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ImageGallery",
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+  data () {
+    return {
+    currentImage: false,
+    isImageModalActive: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.section {
+  position: relative;
+  background: #f7f7f7;
+  color: #111;
+  padding: 4em;
+  text-align: center;
+}
+a {
+  color: #111;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
