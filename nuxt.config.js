8205a1727b1ec7bb926ba6ad2e84085c88286f1e
Mirco <hi@mirco.space>
2021-02-23 02:03:07 +0800

    Trailing slash in url in navbar

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
+  generate: {
+    fallback: true
+  },
   head: {
     title: 'vivai-previdi',
     htmlAttrs: {
