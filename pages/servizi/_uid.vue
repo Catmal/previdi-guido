e2d30ef4e2449c4cd522d421bbe2142b16999d0b
Mirco <hi@mirco.space>
2021-02-23 15:13:34 +0800

    removed svg from services

--- pages/servizi/_uid.vue
+++ pages/servizi/_uid.vue
@@ -7,33 +7,17 @@
       class="background"
       style="position:relative"
     >
-      <transition name="bkg">
-        <svg
-          v-if="show"
-          style="position: sticky; top: 94px"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 640 200"
-        >
-          <path
-            fill="#0e9246"
-            fill-opacity="0"
-            d="M0,192L80,170.7C160,149,320,107,480,122.7C640,139,800,213,960,224C1120,235,1280,181,1360,154.7L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
-          />
-        </svg>
-      </transition>
-      <div style=" margin-top:-28%">
-        <div class="container p-4">
-          <prismic-rich-text :field="document.title" />
-          <div class="card pt-6" style="min-height: 100vh; background: rgba(255,255,255,0.85)">
-            <div
-              v-if="document.content && document.content.length > 0 && document.content[0].text !== ''"
-            >
-              <div class="card-content has-text-centered">
-                <prismic-rich-text :field="document.content" />
-              </div>
+      <div class="container p-4">
+        <prismic-rich-text class="mt-6" :field="document.title" />
+        <div class="card pt-6" style="min-height: 100vh; background: rgba(255,255,255,0.85)">
+          <div
+            v-if="document.content && document.content.length > 0 && document.content[0].text !== ''"
+          >
+            <div class="card-content has-text-centered">
+              <prismic-rich-text :field="document.content" />
             </div>
-            <slice-zone type="service" :uid="$route.params.uid" />
           </div>
+          <slice-zone type="service" :uid="$route.params.uid" />
         </div>
       </div>
     </div>
@@ -83,7 +67,7 @@ export default {
 }
 </script>
 
-<style   scoped>
+<style scoped>
 .home-enter-active,
 .home-leave-active {
   transition: opacity 0.5s;
