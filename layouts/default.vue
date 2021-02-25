ed4d13396656f997d4f0bc193e04ddf87e5fdec7
Mirco <hi@mirco.space>
2021-02-25 13:11:25 +0800

    trailing slash end url in menu links

--- layouts/default.vue
+++ layouts/default.vue
@@ -61,19 +61,19 @@
             active-class="is-active"
             class="has-text-primary"
             tag="router-link"
-            :to="{ path: '/azienda' }"
+            :to="{ path: '/azienda/' }"
           >Azienda</b-navbar-item>
           <b-navbar-item
             active-class="is-active"
             class="has-text-primary"
             tag="router-link"
-            :to="{ path: '/piante' }"
+            :to="{ path: '/piante/' }"
           >Piante</b-navbar-item>
           <b-navbar-item
             active-class="is-active"
             class="has-text-primary"
             tag="router-link"
-            :to="{ path: '/lavori' }"
+            :to="{ path: '/lavori/' }"
           >Lavori</b-navbar-item>
 
           <b-navbar-dropdown active-class="is-active" class="drop" label="Servizi">
@@ -81,62 +81,62 @@
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/aiuole' }"
+              :to="{ path: '/servizi/aiuole/' }"
             >Aiuole</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/parchi-e-giardini' }"
+              :to="{ path: '/servizi/parchi-e-giardini/' }"
             >Parchi e Giardini</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/prati' }"
+              :to="{ path: '/servizi/prati/' }"
             >Prati</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/impianti-irrigazione' }"
+              :to="{ path: '/servizi/impianti-irrigazione/' }"
             >Impianti Irrigazione</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/trattamenti-fitosanitari' }"
+              :to="{ path: '/servizi/trattamenti-fitosanitari/' }"
             >Trattamenti Fitosanitari</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/endoterapia' }"
+              :to="{ path: '/servizi/endoterapia/' }"
             >Endoterapia</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/potatura-aerea' }"
+              :to="{ path: '/servizi/potatura-aerea/' }"
             >Potatura Aerea</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/allestimenti-eventi' }"
+              :to="{ path: '/servizi/allestimenti-eventi/' }"
             >Allestimenti Eventi</b-navbar-item>
             <b-navbar-item
               active-class="is-active"
               class="has-text-primary"
               tag="router-link"
-              :to="{ path: '/servizi/lavori-vari' }"
+              :to="{ path: '/servizi/lavori-vari/' }"
             >Lavori Vari</b-navbar-item>
           </b-navbar-dropdown>
           <b-navbar-item
             active-class="is-active"
             class="has-text-primary"
             tag="router-link"
-            :to="{ path: '/contatti' }"
+            :to="{ path: '/contatti/' }"
           >Contatti</b-navbar-item>
           <b-navbar-item icon="phone" href="https://www.facebook.com/guidoprevidi" target="_blank">
             <img style="max-height: 30px;" src="/facebook.png" />
