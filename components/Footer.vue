932f40e4b55df7770ba68c627cda52b8c99f0ae0
mirco <mirco.cattabriga@gmail.com>
2022-01-28 21:08:20 +0800

    Changed email in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-  <footer class="footer has-background-primary center p-4" style="z-index: 20; opacity: 0.9">
+  <footer
+    class="footer has-background-primary center p-4"
+    style="z-index: 20; opacity: 0.9"
+  >
     <div class="container">
       <div class="columns has-text-white mt-4">
         <div class="column is-one-third has-text-centered-mobile">
@@ -7,7 +10,7 @@
           <h5>0535 97277</h5>
 
           <h5 class="has-text-weight-bold">Email</h5>
-          <h5>info@previdiguido.com</h5>
+          <h5>info@previdiguido.it</h5>
         </div>
         <div class="column is-one-third has-text-centered">
           <h5 class="has-text-weight-bold">Indirizzo</h5>
@@ -15,7 +18,9 @@
           <h5>Massa Finalese</h5>
           <h5>Modena</h5>
         </div>
-        <div class="column is-one-third has-text-right has-text-centered-mobile">
+        <div
+          class="column is-one-third has-text-right has-text-centered-mobile"
+        >
           <h5 class="has-text-weight-bold">Orario Apertura</h5>
           <div v-if="currentMonth > 9 || currentMonth < 4">
             <h5>da lunedi al sabato</h5>
@@ -28,7 +33,12 @@
             <h5>Chiuso sabato e domenica</h5>
           </div>
           <div
-            v-if="(currentMonth > 3 && currentMonth < 11) && currentMonth !==6 && currentMonth !==7"
+            v-if="
+              currentMonth > 3 &&
+              currentMonth < 11 &&
+              currentMonth !== 6 &&
+              currentMonth !== 7
+            "
           >
             <h5>da lunedi al sabato</h5>
             <h5>8.30 – 12.00 | 14.30 – 19.00</h5>
@@ -37,9 +47,9 @@
         </div>
       </div>
       <div class="center">
-        <h5
-          class="has-text-white is-size-6"
-        >Copyright 2021 Previdi Guido - Tutti i diritti riservati</h5>
+        <h5 class="has-text-white is-size-6">
+          Copyright 2021 Previdi Guido - Tutti i diritti riservati
+        </h5>
       </div>
     </div>
   </footer>
@@ -49,8 +59,8 @@ export default {
   computed: {
     currentMonth() {
       return new Date().getMonth() + 1;
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>
