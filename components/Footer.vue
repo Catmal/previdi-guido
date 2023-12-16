3ca710255fce3b4d224af5479356375dec96f587
Mirco <mirco.cattabriga@gmail.com>
2023-12-16 21:03:56 +0100

    Updated opening times

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,5 @@
 <template>
-  <footer
-    class="footer has-background-primary center p-4"
-    style="z-index: 20; opacity: 0.9"
-  >
+  <footer class="footer has-background-primary center p-4" style="z-index: 20; opacity: 0.9">
     <div class="container">
       <div class="columns has-text-white mt-4">
         <div class="column is-one-third has-text-centered-mobile">
@@ -18,28 +15,23 @@
           <h5>Massa Finalese</h5>
           <h5>Modena</h5>
         </div>
-        <div
-          class="column is-one-third has-text-right has-text-centered-mobile"
-        >
+        <div class="column is-one-third has-text-right has-text-centered-mobile">
           <h5 class="has-text-weight-bold">Orario Apertura</h5>
           <div v-if="currentMonth > 9 || currentMonth < 4">
-            <h5>da lunedi al sabato</h5>
+            <h5>da lunedi a venerdì</h5>
             <h5>8.30 – 12.00 | 14.30 – 18.00</h5>
-            <h5>Chiuso la domenica</h5>
+            <h5>Chiuso sabato e domenica</h5>
           </div>
           <div v-if="currentMonth === 7 || currentMonth === 8">
             <h5>da lunedi al venerdi</h5>
             <h5>8.30 – 12.00 | 16.30 – 19.00</h5>
             <h5>Chiuso sabato e domenica</h5>
           </div>
-          <div
-            v-if="
-              currentMonth > 3 &&
-              currentMonth < 11 &&
-              currentMonth !== 6 &&
-              currentMonth !== 7
-            "
-          >
+          <div v-if="currentMonth > 3 &&
+            currentMonth < 10 &&
+            currentMonth !== 6 &&
+            currentMonth !== 7
+            ">
             <h5>da lunedi al sabato</h5>
             <h5>8.30 – 12.00 | 14.30 – 19.00</h5>
             <h5>Chiuso la domenica</h5>
@@ -57,7 +49,7 @@
 <script>
 export default {
   computed: {
-    currentMonth() {
+    currentMonth () {
       return new Date().getMonth() + 1;
     },
   },
